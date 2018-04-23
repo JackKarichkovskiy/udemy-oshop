8b11f9ac2bc952aa0e3caf37c621eb7bd4298ecb
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-23 11:59:36 +0200

    lesson 278

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAvGIV6ZdK3_IWewMV-a2YaZVXOsKnaj0c",
+    authDomain: "oshop-9282b.firebaseapp.com",
+    databaseURL: "https://oshop-9282b.firebaseio.com",
+    projectId: "oshop-9282b",
+    storageBucket: "",
+    messagingSenderId: "674151771256"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAvGIV6ZdK3_IWewMV-a2YaZVXOsKnaj0c",
+    authDomain: "oshop-9282b.firebaseapp.com",
+    databaseURL: "https://oshop-9282b.firebaseio.com",
+    projectId: "oshop-9282b",
+    storageBucket: "",
+    messagingSenderId: "674151771256"
+  }
 };
