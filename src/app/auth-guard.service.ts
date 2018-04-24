a866c3643880dbd67d07a2991926b8977f4790c9
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-24 11:34:38 +0200

    lesson 293

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -5,15 +5,15 @@ import { AuthService } from './auth.service';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
-      if(user) return true;
+      if (user) return true;
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
       return false;
     })
   }
