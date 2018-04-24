98e58fe32fb8d2627d936860db17b7faaf2a51e9
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-24 11:15:16 +0200

    lesson 291

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase';
-import { Observable } from 'rxjs/Observable';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'bs-navbar',
@@ -10,13 +9,11 @@ import { Observable } from 'rxjs/Observable';
 })
 export class BsNavbarComponent {
 
-  user$: Observable<firebase.User>;
-
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = afAuth.authState;
-   }
+  constructor(private auth: AuthService) {
+  }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
+
 }
