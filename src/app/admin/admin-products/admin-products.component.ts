f3a2c4458abd7093dd18c4e07f5775fb1267366a
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-25 18:01:39 +0200

    lesson 315

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -12,7 +12,6 @@ import { DataTableResource } from 'angular5-data-table';
 export class AdminProductsComponent implements OnInit, OnDestroy {
 
   products: Product[];
-  filteredProducts: any[];
   subscription: Subscription;
   tableResource: DataTableResource<Product>;
   items: Product[] = [];
@@ -21,7 +20,6 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   constructor(private productService: ProductService) {
     this.subscription = this.productService.getAll()
       .subscribe(products => {
-        this.filteredProducts = this.products = products;
         this.initializeTable(products);
       });
   }
@@ -34,9 +32,11 @@ export class AdminProductsComponent implements OnInit, OnDestroy {
   }
 
   filter(query: string) {
-    this.filteredProducts = query ?
+    let filteredProducts = query ?
       this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
       this.products;
+
+    this.initializeTable(filteredProducts);
   }
 
   reloadItems(params) {
