d2b432b86b957f167c709d1110c667fb739aa4c2
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-26 17:53:09 +0200

    lesson 342

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -27,6 +27,7 @@ export class ShoppingCart {
     }
 
     getQuantity(product: Product) {
+        console.log("product", product);
         let item = this.itemsMap[product.$key];
         return item ? item.quantity : 0;
     }
