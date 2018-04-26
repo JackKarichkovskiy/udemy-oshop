f3f0eece1992ead3154e0f34d36d3f81f110a771
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-26 16:49:16 +0200

    lesson 336

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Product } from './models/product';
 import 'rxjs/add/operator/take';
+import { ShoppingCart } from './models/shopping-cart';
 
 @Injectable()
 export class ShoppingCartService {
@@ -16,7 +17,7 @@ export class ShoppingCartService {
     this.updateItemQuantity(product, -1);
   }
 
-  async getCart() {
+  async getCart(): Promise<FirebaseObjectObservable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId);
   }
