0e84ec88574446766772e7b5375891fcea657349
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-26 17:33:51 +0200

    lesson 340

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -5,8 +5,10 @@ export class ShoppingCart {
     items: ShoppingCartItem[] = [];
 
     constructor(public itemsMap: { [key: string]: ShoppingCartItem }) {
-        for (let productId in itemsMap)
-            this.items.push(itemsMap[productId]);
+        for (let productId in itemsMap) {
+            let item = itemsMap[productId];
+            this.items.push(new ShoppingCartItem(item.product, item.quantity));
+        }
     }
 
     get totalItemsCount() {
@@ -14,5 +16,12 @@ export class ShoppingCart {
         for (let productId in this.itemsMap)
             count += this.itemsMap[productId].quantity;
         return count;
+    }
+
+    get totalPrice() {
+        let sum = 0;
+        for (let productId in this.items)
+            sum += this.items[productId].totalPrice;
+        return sum;
     }
 }
