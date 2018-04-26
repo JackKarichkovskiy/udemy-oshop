5e9a1e5bdf4f05beaf46eb48d749d1f326714cab
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-04-26 14:04:22 +0200

    lesson 330

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -6,7 +6,25 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create() {
+
+  private async getOrCreateCart() {
+    let cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+      let result = await this.create();
+
+      localStorage.setItem('cartId', result.key);
+
+      cartId = result.key;
+    }
+    
+    return this.getCart(cartId);
+  }
+
+  private getCart(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private create() {
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     });
